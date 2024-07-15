534f49b682710fdaae700bbbb9f944575cb36005
Petteri Zitting <petteri.zitting@solita.fi>
2024-07-15 08:48:18 +0300

    Add a cleanup script

---
+++ scripts/clean.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Fail fast
+set -euo pipefail
+
+# Ensure we are working in the correct directory (repo root)
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  SCRIPT_DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$SCRIPT_DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SCRIPT_DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+REPO_ROOT=$(realpath "$SCRIPT_DIR/..")
+
+BUILD_DIR="$REPO_ROOT/build"
+
+echo "Deleting $BUILD_DIR"
+
+rm -r "$BUILD_DIR"
